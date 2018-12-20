51f2418e469a9a33189b55c9d0c3fb28c22dd960
Glenn Purtill <glennpurtill@Glenns-MacBook-Pro.local>
2018-12-20 13:26:12 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,12 @@ let maxl = 8;
 let curAmountCols = 8;
 let playstate = false;
 
+var chorus = new tone.Chorus(4, 2.5, 0.5);
 var vol = new tone.Volume(-10);
 var dist = new tone.Distortion(0.9);
 var reverb = new tone.JCReverb(0.9);
 var delay = new tone.FeedbackDelay(0.8);
-let bassSynth = new tone.FMSynth().chain(delay, reverb, dist, tone.Master);
+let bassSynth = new tone.FMSynth().chain(delay, chorus, reverb, dist, tone.Master);
 let arpeggioSynth = new tone.MonoSynth(
 
   {
@@ -45,7 +46,7 @@ let arpeggioSynth = new tone.MonoSynth(
     "exponent"  : 2
     }
   }
-).chain( vol, delay, reverb, dist, tone.Master);
+).chain( vol, delay, chorus, reverb, dist, tone.Master);
 
 let tempArr = ['C4','D4','E4','F4','G4','A4','B4']
 let tempArpeggio = ['','','','','','','','']
@@ -146,7 +147,7 @@ export class AppComponent {
   distortionS = "on"
   reverbS = "on"
   delayS = "on"
-  
+  chorusS = "on"
 	ionian = this.makeScale(ionian_SCALE);
 	aeolian = this.makeScale(aeolian_SCALE);
 	romanian = this.makeScale(romanian_SCALE);
@@ -263,24 +264,24 @@ export class AppComponent {
   }
 
   chordClicked(chord, num, c){
-    
+   
     this[chord] = num
+    console.log(chord + num + c)
     let curAct = "curActiveC" + c
     console.log(document.getElementById(chord+num))
     if(this[curAct] != ''){
-      document.getElementById(this[curAct]).setAttribute("style", "background-color: rgb(0, 0, 0); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
+      document.getElementById(this[curAct]).setAttribute("style", "background-color: rgb(47, 47, 47); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
     }
 
     let curChord = ''
-      curChord = chord + num
-    console.log(chord+"1"+num)
+    curChord = chord + num
     if(chord == 'chord11'){
       document.getElementById(chord+'1'+num).style.backgroundColor = 'red';
+      curChord = chord+"1"+num
     }
     else {
       document.getElementById(chord+num).style.backgroundColor = 'red';
     }
-    // document.getElementById(chord+'1'+num).style.backgroundColor = 'red';
     this[curAct] = curChord
 	this.autoupdate();
   }
@@ -344,6 +345,20 @@ export class AppComponent {
 	this.autoupdate();
    }
 
+   chorusSwitch(val){
+    if(this.chorusS == "off"){
+    document.getElementById("chorus").innerHTML="ON";
+    document.getElementById("chorus").style.backgroundColor = 'red';
+    this.chorusS = "on";
+    }
+    else{
+      document.getElementById("chorus").innerHTML="OFF";
+       document.getElementById("chorus").style.backgroundColor = '';
+      this.chorusS = "off";
+     }
+ this.autoupdate();
+  }
+
   changeTonicRoot(val){
     this.tonicRoot = val
     if(this.curTonicRoot != ''){
@@ -406,7 +421,7 @@ export class AppComponent {
         // // this.newRowInnerHtml.toString()
         element.addEventListener("click", this.here.bind(this));
         element.classList.add("col-sm-11")
-        element.setAttribute("style", "background-color: rgb(0, 0, 0); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
+        element.setAttribute("style", "background-color: rgb(47, 47, 47); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
         this.check[at]==true
       }
     }
@@ -426,7 +441,7 @@ export class AppComponent {
         let html = "here"
         // let element = (<HTMLElement><any>document.createElement("button"));
         if(i==10){
-          html = '<button id="chord'+ (i+1).toString() + '1' + this.curAmountRows.toString() + '" value="chord'+ (i+1).toString() + ',' + this.curAmountRows.toString() + ',' + (i+1).toString() +'"></button>'  // chordClicked('chord16', 2, 16)
+          html = '<button id="chord'+ (i+1).toString() + '1' + this.curAmountRows.toString() + '" value="chord'+ (i+1).toString() + '1' + ',' + this.curAmountRows.toString() + ',' + (i+1).toString() +'"></button>'  // chordClicked('chord16', 2, 16)
         }
         else{
           html = '<button id="chord'+ (i+1).toString() + this.curAmountRows.toString() + '" value="chord'+ (i+1).toString() + ',' + this.curAmountRows.toString() + ',' + (i+1).toString() +'"></button>'  // chordClicked('chord16', 2, 16)
@@ -445,7 +460,7 @@ export class AppComponent {
         element.innerHTML = this.newRowInnerHtml.toString() + "(" + this.octives + ")"
         element.classList.add("col-sm-11")
         element.addEventListener("click", this.here.bind(this));
-        element.setAttribute("style", "background-color: rgb(0, 0, 0); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
+        element.setAttribute("style", "background-color: rgb(47 ,47, 47); border: none; color: white; padding: 5px; text-align: center; text-decoration: none; display: inline-block; font-size: 12px; margin: 4px 2px; cursor: pointer; border-radius: 12px; width: 80px;")
       }
       this.newRowInnerHtml++
     }
@@ -460,7 +475,7 @@ playCurrentTrack(){
   var distSlideVal = parseFloat((<HTMLInputElement>document.getElementById("distSlide")).value);
   var reverbSlideVal = parseFloat((<HTMLInputElement>document.getElementById("reverbSlide")).value);
   var delaySlideVal = parseFloat((<HTMLInputElement>document.getElementById("delaySlide")).value);
-
+  var chorusSlideVal = parseFloat((<HTMLInputElement>document.getElementById("chorusSlide")).value);
 	if(this.distortionS == "off"){
 		dist.wet.value = 0;
 	}
@@ -478,6 +493,12 @@ playCurrentTrack(){
 	}
 	else{
 		delay.wet.value =  (delaySlideVal/100);
+  }
+  if(this.chorusS == "off"){
+		chorus.wet.value = 0;
+	}
+	else{
+		chorus.wet.value =  (chorusSlideVal/100);
 	}
   console.log(this.bpm)
   let arpeggioSpeed = this.bpm
@@ -597,7 +618,7 @@ playCurrentTrack(){
   var distSlideVal = parseFloat((<HTMLInputElement>document.getElementById("distSlide")).value);
   var reverbSlideVal = parseFloat((<HTMLInputElement>document.getElementById("reverbSlide")).value);
   var delaySlideVal = parseFloat((<HTMLInputElement>document.getElementById("delaySlide")).value);
-
+  var chorusSlideVal = parseFloat((<HTMLInputElement>document.getElementById("chorusSlide")).value);
 	if(this.distortionS == "off"){
 		dist.wet.value = 0;
 	}
@@ -615,6 +636,12 @@ playCurrentTrack(){
 	}
 	else{
 		delay.wet.value =  (delaySlideVal/100);
+  }
+  if(this.chorusS == "off"){
+		chorus.wet.value = 0;
+	}
+	else{
+		chorus.wet.value =  (chorusSlideVal/100);
 	}
   console.log(this.bpm)
   let arpeggioSpeed = this.bpm
@@ -657,6 +684,7 @@ playCurrentTrack(){
     else{
       c = "chord" + c
     }
+    console.log(this.chord111)
     let d = "ionian"
     let example = "D"
     //get mode
@@ -665,7 +693,7 @@ playCurrentTrack(){
     let tr = this.tonicRoot
     tempArr[num] = this[mode][instrument][tr][this[c] - 1] //dynamically changing scale
   }
-
+  console.log(tempArr)
   for (let num =0 ; num<maxl; num++){
     // let d = (num + 1) + ""
     // var c = "value" + d
@@ -785,7 +813,8 @@ stop() {
 		// audio
 		this.distortionSwitch("off");
 		this.reverbSwitch("off");
-		this.delaySwitch("off");
+    this.delaySwitch("off");
+    this.chorusSwitch("off");
     this.distSlide();
   }
 
